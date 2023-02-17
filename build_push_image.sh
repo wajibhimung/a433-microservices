642698a1b590c64921f186870732424894b0d830
Wibowo Laksono <wajibhimung@gmail.com>
2023-02-17 18:56:15 +0800

    add push to  GitHub Packages

--- build_push_image.sh
+++ build_push_image.sh
@@ -10,15 +10,18 @@ docker images
 
 echo "\nMengubah nama docker image sesuai format docker registry"
 #Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat)
-docker tag item-app:v1 wajibhimung/item-app:v1
+#docker tag item-app:v1 wajibhimung/item-app:v1
+docker tag item-app:v1 ghcr.io/wajibhimung/item-app:v1
 
 echo "\nLogin ke Docker Hub"
 #Login ke Docker Hub 
-echo $PASSWORD_DOCKER_HUB | docker login -u wajibhimung --password-stdin
+#echo $PASSWORD_DOCKER_HUB | docker login -u wajibhimung --password-stdin
+echo $CR_PAT | docker login ghcr.io -u wajibhimung --password-stdin
 
 echo "\nMengunggah image ke Docker Hub ..."
 #Mengunggah image ke Docker Hub
-docker push wajibhimung/item-app:v1
+#docker push wajibhimung/item-app:v1
+docker push ghcr.io/wajibhimung/item-app:v1
 
 
 

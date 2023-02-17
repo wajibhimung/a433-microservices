f860732374fc788e63d4ba3df01f4e9ee69511e1
Wibowo Laksono <wajibhimung@gmail.com>
2023-02-17 18:14:32 +0800

    Add comment

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
+#Menggunakan base image Node.js versi 14.
 FROM node:14-alpine
-
+#Menentukan working directory untuk container /app.
 WORKDIR /app
+#Menyalin seluruh source code ke working directory di container
 COPY . .
+#Menentukan agar aplikasi berjalan dalam production mode dan menggunakan container bernama item-dbsebagai database host
 ENV NODE_ENV=production DB_HOST=item-db
-
+#Menginstal dependencies untuk production dan kemudian build aplikasi
 RUN npm install --production --unsafe-perm && npm run build
-
+#Menjalankan aplikasi
 CMD ["npm", "start"]
-
+#Ekspos port yang digunakan oleh aplikasi adalah 8080
 EXPOSE 8080
--- build_push_image.sh
+++ build_push_image.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
+
+echo "Membuat docker image ... "
 #Membuat Docker image dari Dockerfile, dengan nama image item-app & tag v1
 docker build -t item-app:v1 .
 
+echo "\nList docker images"
 #Melihat daftar image di lokal.
 docker images
 
+echo "\nMengubah nama docker image sesuai format docker registry"
 #Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat)
 docker tag item-app:v1 wajibhimung/item-app:v1
 
+echo "\nLogin ke Docker Hub"
 #Login ke Docker Hub 
 echo $PASSWORD_DOCKER_HUB | docker login -u wajibhimung --password-stdin
 
+echo "\nMengunggah image ke Docker Hub ..."
 #Mengunggah image ke Docker Hub
 docker push wajibhimung/item-app:v1
 
